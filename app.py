3389dc280fafb69641077f183963fd919d162b61
Марк Чернышов <mark.chernyshov.2003@mail.ru>
2024-09-04 18:22:13 +0700

    Стили

--- app.py
+++ app.py
@@ -35,9 +35,13 @@ def author():
 @app.route('/lab1/oak')
 def oak():
     path = url_for("static", filename="oak.jpg")
+    css_path = url_for("static", filename="lab1.css")
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="''' + css_path + '''">
+    </head>
     <body>
         <h1>Дуб</h1>
         <img scr="'''+ path +'''">
