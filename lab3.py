23577825078706b9eda929d618fea3ffd9531fe0
Чернышов Марк home <mark.chernyshov.2003@mail.ru>
2025-02-19 13:27:37 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,7 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, make_response, redirect
 
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
-    return render_template('lab3.html')
+    name = request.cookies.get('name')
+    if not name:  # если name пустое или None
+        name = "Неизвестный"
+    
+    name_color = request.cookies.get('name_color')
+    age = request.cookies.get('age')
+    if not age:  # если age пустое или None
+        age = "Возраст не указан"
+    
+    return render_template('lab3/lab3.html', name=name,
+                            name_color=name_color, age=age)
+
+@lab3.route('/lab3/cookie')
+def cookie():
+    resp = make_response(redirect('/lab3'))
+    resp.set_cookie('name', 'Max', max_age=5)
+    resp.set_cookie('age', '21')
+    resp.set_cookie('name_color', 'red')
+    return resp
+
+@lab3.route('/lab3/del_cookie')
+def del_cookie():
+    resp = make_response(redirect('/lab3'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    return resp
