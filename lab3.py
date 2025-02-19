aa22f8e7ce45c2ef9979c374272cf77930192438
Чернышов Марк home <mark.chernyshov.2003@mail.ru>
2025-02-19 22:22:58 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -46,3 +46,31 @@ def form1():
         errors['age'] = 'Заполните поле возраста!'
 
     return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'cofee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    
+    return render_template('lab3/pay.html', drink = drink, price = price)
+@lab3.route('/lab3/success_pay', methods=['GET'])
+def success_pay():
+    drink = request.args.get('drink')
+    price = request.args.get('price')
+    return render_template('lab3/success_pay.html', drink=drink, price=price)
