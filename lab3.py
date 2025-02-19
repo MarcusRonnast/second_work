cb6bc7a8e31723133faae3ac84770d483fddddd2
Чернышов Марк home <mark.chernyshov.2003@mail.ru>
2025-02-19 21:51:19 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -35,7 +35,14 @@ def del_cookie():
 
 @lab3.route('/lab3/form1')
 def form1():
+    errors = {}
     user = request.args.get('user')
     age = request.args.get('age')
     sex = request.args.get('sex')
-    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
+
+    if user == '':
+        errors['user'] = 'Заполните поле имени!'
+    if age == '':
+        errors['age'] = 'Заполните поле возраста!'
+
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
