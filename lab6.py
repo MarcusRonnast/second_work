13d447a8ee0f4aa2125f968b5e1454cffbf3440a
Чернышов Марк home <mark.chernyshov.2003@mail.ru>
2025-03-02 01:52:37 +0700

    Стоимость аренды

--- lab6.py
+++ lab6.py
@@ -1,21 +1,20 @@
-from flask import Blueprint, render_template, request, redirect, session, current_app
+from flask import Blueprint, render_template, request, session
 
 lab6 = Blueprint('lab6', __name__)
 
-offices=[]
-for i in range(1,11):
-    offices.append({'number': i, 'tenant': ''})
+offices = []
+for i in range(1, 11):
+    offices.append({'number': i, 'tenant': '', 'price': 1000})
 
 @lab6.route('/lab6/')
 def main():
     return render_template('lab6/lab6.html')
 
-
 @lab6.route('/lab6/json-rpc-api/', methods=['POST'])
 def api():
     data = request.json
     id = data['id']
-    if data['method'] =='info':
+    if data['method'] == 'info':
         return {
             'jsonrpc': '2.0',
             'result': offices,
@@ -24,13 +23,13 @@ def api():
     login = session.get('login')
     if not login:
         return {
-        'jsonrpc': '2.0',
-        'error': {
-            'code': 1,
-            'message': 'Unauthorized'
-        },
-        'id': id
-    }
+            'jsonrpc': '2.0',
+            'error': {
+                'code': 1,
+                'message': 'Unauthorized'
+            },
+            'id': id
+        }
     
     if data['method'] == 'booking':
         office_number = data['params']
@@ -46,46 +45,50 @@ def api():
                         'id': id
                     }
                 office['tenant'] = login  # Устанавливаем арендатора
-                return{
+                return {
+                    'jsonrpc': '2.0',
                     'result': 'success',
                     'id': id
                 }
-            return {
+        # Если офис не найден
+        return {
             'jsonrpc': '2.0',
             'error': {
                 'code': 3,
                 'message': 'Office not found'
             },
             'id': id
-            }
+        }
+    
     if data['method'] == 'cancellation':
-            office_number = data['params']
-            for office in offices:
-                if office['number'] == office_number:
-                    if not office['tenant']:
-                        return {
-                            'jsonrpc': '2.0',
-                            'error': {
-                                'code': 3,
-                                'message': 'Office not booked'
-                            },
-                            'id': id
-                        }
-                    if office['tenant'] != login:
-                        return {
-                            'jsonrpc': '2.0',
-                            'error': {
-                                'code': 4,
-                                'message': 'You cannot cancel someone else\'s booking'
-                            },
-                            'id': id
-                        }
-                    office['tenant'] = ''  # Освобождаем офис
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if not office['tenant']:
                     return {
                         'jsonrpc': '2.0',
-                        'result': 'success',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office not booked'
+                        },
                         'id': id
-                    }   
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'You cannot cancel someone else\'s booking'
+                        },
+                        'id': id
+                    }
+                office['tenant'] = ''  # Освобождаем офис
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+    
     return {
         'jsonrpc': '2.0',
         'error': {
