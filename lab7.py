c0fdd32b0366f7db96c0db8529f032b28aa7952b
Чернышов Марк home <mark.chernyshov.2003@mail.ru>
2025-03-02 03:09:09 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -61,3 +61,12 @@ def del_film(id):
         abort(404)  # Возвращаем ошибку 404, если id выходит за пределы
     del films[id]
     return '', 204  # Возвращаем пустой ответ с кодом 204 (No Content)
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if id < 0 or id >= len(films):
+        abort(404)
+    film = request.get_json()
+    films[id] = film
+    return films[id]
