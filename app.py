5a47ab62b9c35470fc4fd7d1da26bd403964e934
Чернышов Марк home <mark.chernyshov.2003@mail.ru>
2025-03-03 10:56:38 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -7,40 +7,10 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
-import os
-from flask_sqlalchemy import SQLAlchemy
-from db import db
-from os import path
-from flask_login import LoginManager
-from db.models import users
+from lab9 import lab9
+
 app = Flask(__name__)
 
-login_manager = LoginManager()
-login_manager.login_view = 'lab8.login'
-login_manager.init_app(app)
-
-@login_manager.user_loader
-def load_users(user_id):
-    return users.query.get(int(user_id))
-
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'супер-секретно-секретный-секрет')
-app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
-
-if app.config['DB_TYPE'] == 'postgres':
-    db_name = 'mark_chernyshov_orm'
-    db_user = 'mark_chernyshov_orm'
-    db_password = 'sheesh'
-    host_ip = '127.0.0.1'
-    host_port = 5432
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
-
-else:
-    dir_path = path.dirname(path.realpath(__file__))
-    db_path = path.join(dir_path, "mark_chernyshov_orm.db")
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
-
-db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -50,6 +20,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.route("/")
 @app.route("/index")
@@ -87,6 +58,9 @@ def main_page():
                             </li>
                             <li>    
                                 <a href="/lab8">Восьмая лабораторная</a>
+                            </li>
+                            <li>    
+                                <a href="/lab9">Девятая лабораторная</a>
                             </li>
                         </ul>
                     </main>
